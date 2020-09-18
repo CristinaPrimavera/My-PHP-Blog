e554d5c5132db215e6b2e49194877a00799cd7fb
CristinaPrimavera <primaveracristina@gmail.com>
2020-09-18 10:33:43 +0200

    Vanilla JS version

--- my-js/my-script.js
+++ my-js/my-script.js
@@ -13,5 +13,25 @@ $("a.nav-link.delete").on("click", function(e){
     }
 });
 
+//Or with vanilla JS:
+
+// const deleteButton = document.querySelector('.delete');
+//
+// if (deleteButton) {
+//     deleteButton.addEventListener('click', e => {
+//         e.preventDefault();
+//         if (confirm('Are you sure?')) {
+//             const deleteForm = document.createElement('form');
+//
+//             deleteForm.setAttribute('method', 'post');
+//             deleteForm.setAttribute('action', e.target.href);
+//             document.body.append(deleteForm);
+//             deleteForm.submit();
+//         }
+//     });
+// }
+
+
+
 /*It's not working as expected. After clicking the delete button and saying yes to the confirmation message,
 * I'm still being redirected to the delete file page*/
